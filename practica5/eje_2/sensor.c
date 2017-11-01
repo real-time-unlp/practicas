d1499cde32d8ebb8ff118e11d87f4719a02086ca
Ramiro <ramiroromero94@gmail.com>
2017-11-01 15:11:30 -0300

    Sensor.

---
+++ practica5/eje_2/sensor.c
@@ -0,0 +1,48 @@
+#define _WITH_GETLINE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdint.h>
+
+void die()
+{
+	perror(NULL);
+	exit(-1);
+}
+
+void wait_ns(long ns)
+{
+	struct timespec sleep_params;
+	sleep_params.tv_sec = ns / 1000000000L;
+	sleep_params.tv_nsec = ns % 1000000000L;
+	nanosleep(&sleep_params, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	char *line = NULL;
+	size_t linecap;
+
+	if (argc != 2)
+		return -1;
+
+	FILE * file = fopen(argv[1], "r");
+	if (file == NULL)
+		die();
+
+
+	while (getline(&line, &linecap, file) > 0) {
+		float temp;
+		long ns;
+		sscanf(line, "%f\t%ld", &temp, &ns);
+		fprintf(stderr, "%f\t%ld\n", temp, ns);
+		wait_ns(ns);
+		write(0, &temp, sizeof temp);
+	}
+
+	fprintf(stderr, "\nTerminado.\n");
+	free(line);
+	fclose(file);
+	return 0;
+}
